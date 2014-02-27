437973c87e6b09c0e615b2ad2b35fa93826cac07
Ricardo Rubio <rickarubio@gmail.com>
2014-02-27 11:46:59 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edufyme::Application.config.secret_token = 'a632dfcb40cedbfa3e15df85c0b7a790599f1b7bc462bab549fae59ef40b288e827c66a000e94b7b7d37bd3afb7c23a4a0a1ae4bd0fe1fb24e79a0e947e7c3f8'
