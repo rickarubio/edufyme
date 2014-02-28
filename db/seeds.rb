6772b95c8907e6bced645ccfd9eccce4df266f04
Ricardo Rubio <rickarubio@gmail.com>
2014-02-27 18:32:13 -0800

    began work on seed data generation, #seed_schools implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def seed_schools
+  school_images = [
+    "http://studentcompetitions-general.s3.amazonaws.com/cyp-2014/img/lundsuniversitet_-_b.jpg",
+    "http://www.studyco.com/upload/images/oxford_university(1).jpg",
+    "http://www.hdpaperwall.com/wp-content/uploads/2013/11/Harvard-University.jpg",
+    "http://www.huntington-study-group.org/Portals/0/University%20of%20tennessee.jpg"]
+  5.times do
+    School.create(
+      name: (Faker::Company.name + " University"),
+      school_url: ("http://www." + Faker::Internet.domain_name)
+      school_img_url: school_images.sample
+      )
+  end
+end
+
+def seed_courses
+  50.times do
+    Course.create(
+      )
+  end
+end
+
+
+
+def seed_categories
+end
+
+def seed_teachers
+end
+
+def seed_users
+end
+
+def seed_course_users
+end
+
+def seed_course_teachers
+end
+
+def run_once
+  seed_schools
+  seed_courses
+  seed_categories
+  seed_teachers
+  seed_users
+  seed_course_users
+  seed_course_teachers
+end
+
+run_once
