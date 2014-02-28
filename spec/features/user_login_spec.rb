e8e0724770dd7eeeb72b485f267726b18b9d9829
Ricardo Rubio <rickarubio@gmail.com>
2014-02-28 10:24:56 -0800

    deletes unnecessary test folders

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "User Login" do
+  describe "Home" do
+    context "user should be able to see a login link" do
+      it "has a link to the user login page" do
+        visit root_path
+        expect(page).to have_content "Login"
+        expect(page).to have_selector "a"
+      end
+    end
+  end
+end
