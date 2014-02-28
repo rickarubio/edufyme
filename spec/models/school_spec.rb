8a98aa2e08eb91ab7a4c56761c18c1709a40c030
Apprentice <apprentice@dbc19.local>
2014-02-28 11:22:37 -0800

    Adds model tests for school, teacher, and users.

---
+++ spec/models/school_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe School do
+  it { should respond_to :name }
+  it { should respond_to :school_url }
+  it { should respond_to :school_img_url }
+  it { should respond_to :courses }
+end
