868e7770d3462235f9642627eed91ac2623999fc
Apprentice <apprentice@dbc20.local>
2014-02-28 19:36:46 -0800

    began work on Overlay#populate JS method

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -7,6 +7,7 @@ bindEvents = function() {
   $('.courses-container').on('click', '.course', function(e) {
     e.preventDefault();
     Overlay.display();
+    Overlay.populate(this);
   }),
 
   $('.exit-modal').on('click', function(e){
@@ -26,6 +27,11 @@ var Overlay = (function() {
     hide: function(){
      $('.overlay').css('display', 'none');
      $('.modal').css('display', 'none');
+    },
+
+    populate: function(course) {
+      var course_id = $(course).attr('data-course-id');
+      alert(course_id);
     }
   }
 
