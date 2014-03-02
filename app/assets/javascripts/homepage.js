087b7ea486dfd52519a78a9ac453dade4c86dc02
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-03-01 23:49:41 -0800

    Refactored Javascript to uphold modular design pattern and began the CSS to star a course.

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(function() {
   bindEvents();
-  $('#loginmodaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
-  $('#signupmodaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
 })
 
-
 var bindEvents = function() {
+
+  $('#loginmodaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
+  $('#signupmodaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
   $('.courses-container').on('click', '.course', function(e) {
     e.preventDefault();
     Overlay.display();
@@ -15,6 +15,17 @@ var bindEvents = function() {
   $('.overlay').on('click', function(e){
     e.preventDefault();
     Overlay.hide();
+  }),
+
+  //BUG: do this for particular course, not all at once
+
+  $('.course').hover(function(e){
+    e.preventDefault();
+    console.log('hereee')
+    //make star appear where course.id = course.data
+    $('.star').fadeTo(30, 1.0);
+  }, function() {
+    $('.star').fadeOut(30);
   })
 
   // $("#modaltrigger").on('click', function(e) {
@@ -24,7 +35,7 @@ var bindEvents = function() {
 }
 
 var Overlay = (function() {
-  //private _methods and _vars
+
   var _populateCourseInfo = function(course) {
     $('.overlay-course-title').text(course.title);
     $('.modal img').attr('src', course.course_img_url);
@@ -36,32 +47,32 @@ var Overlay = (function() {
     $('.overlay-teacher img').attr('src', 'http://www.biography.com/imported/images/Biography/Images/Profiles/T/Mark-Twain-9512564-1-402.jpg');
     $('.overlay-teacher-bio').text('Insert Teacher Bio Here!' + course.description.slice(1, 450) + '...');
   }
-  return {
-    display: function() {
-      $('.overlay').fadeTo(300, 0.5);
-      $('.modal').fadeTo(300, 1.0);
-      // var modal_width = $('.modal').outerWidth();
-      // var mwidth = (modal_width/4);
-      // $('.modal').css('margin-left', mwidth + "px");
-    },
 
-    hide: function(){
+  var _display = function() {
+    $('.overlay').fadeTo(300, 0.5);
+    $('.modal').fadeTo(300, 1.0);
+  }
+
+  var _hide = function() {
      $('.overlay').fadeOut(300);
      $('.modal').fadeOut(300);
-    },
+  }
 
+  var _requestCourseInfo = function(course) {
+    var course_id = $(course).attr('data-course-id');
+    $.ajax({
+      url: '/courses/' + course_id
+    }).done(function(course){
+      _populateCourseInfo(course);
+    }).fail(function(){
+      alert('failed');
+    })
+  }
 
-    requestCourseInfo: function(course) {
-      var course_id = $(course).attr('data-course-id');
-      $.ajax({
-        url: '/courses/' + course_id
-      }).done(function(course){
-        _populateCourseInfo(course);
-      }).fail(function(){
-        alert('failed');
-      })
-    }
-
+  return {
+    display: _display,
+    hide: _hide,
+    requestCourseInfo: _requestCourseInfo
   }
 
 }());
