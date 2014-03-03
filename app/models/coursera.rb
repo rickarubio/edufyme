b6eeb3bed3552cdbb5363239dad0d9214083f052
Apprentice <apprentice@DBC-Infinity-2.local>
2014-03-02 21:08:03 -0800

    Improving Coursera data importing logic

--- app/models/coursera.rb
+++ app/models/coursera.rb
@@ -10,10 +10,21 @@ class Coursera
         course_img_url: course["photo"],
         start_date: nil,
         end_date: nil,
-        short_description: course["short_description"]
+        short_description: course["short_description"],
+        school_id: Coursera.populate_school(course)
       }
     end
     formatted_courses.each {|course| Course.create(course)}
+  end
+
+private
+  def self.populate_school(course)
+    school = {
+      name: course["universities"][0]["name"],
+      school_url: course["universities"][0]["home_link"],
+      school_img_url: course["universities"][0]["logo"]
+    }
+    School.create(school).id
   end
 
   def self.populate_course_descriptions(short_name)
@@ -22,5 +33,9 @@ class Coursera
     description = doc.css('p').inner_text
     description.gsub("\n", '').split(/ {2,}/).join('')
   end
-
 end
+
+# course is currently missing:
+## category_id
+## start date
+## end_date
