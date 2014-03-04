af25fa4fa12ad592cfd6a19363d35126e7299ebd
Spencer Smitherman & Ricardo A. Rubio <pair+bigspencey.rickarubio@devbootcamp.com>
2014-03-03 23:16:14 -0800

    Fixed school display bug

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,9 @@
 class School < ActiveRecord::Base
   attr_accessible :name, :school_url, :school_img_url
   has_many :courses
+
+  def self.short_school_name(school_name)
+    max_school_name_length = 39
+    school_name.slice(0, max_school_name_length)
+  end
 end
