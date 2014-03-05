98e8193a26aed7484bceb2a6da9bafe7fbb6e5a7
Spencer Smitherman <null>
2014-03-05 11:45:42 -0800

    Unit testing complete

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,28 +1,5 @@
 require 'spec_helper'
 
-# describe Course do
-#   let(:course) { FactoryGirl.create(:course) }
-#   it "has a title attribute" do
-#     expect(course).to respond_to :title
-#   end
-
-#   it "has a description attribute" do
-#     expect(course).to respond_to :description
-#   end
-
-#   it "has an url attribute" do
-#     expect(course).to respond_to :course_url
-#   end
-
-#   it "has a start date attribute" do
-#     expect(course).to respond_to :start_date
-#   end
-
-#   it "has at least one instructor" do
-#     expect(course).to respond_to :teachers
-#   end
-
-# end
 describe Course do
   it { should respond_to :title}
   it { should respond_to :description}
