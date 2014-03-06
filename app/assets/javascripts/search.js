278a93486c2734addf73040495a5a66a4679102e
Ricardo Rubio <rickarubio@gmail.com>
2014-03-05 19:16:42 -0800

    Implements methods needed to display search results on homepage

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -28,15 +28,42 @@ var Search = (function(){
       $('.search input').val("");
     },
 
-    submitQuery: function() {
-      alert('query sent - not really');
+    emptyCourseContainer: function() {
+      $('div.courses-container').empty();
+      $('.featured-courses-title h3').text("Search Results");
+    },
+
+    appendResults: function(data) {
+      // unbind infinite scrolling
+      $(window).off('scroll');
+      // build a course card
+      var $courseTemplate;
+      $.each(data, function(idx, course){
+        $courseTemplate = $('.course-template').clone();
+        $courseTemplate.attr('data-course-id', course.id);
+        $courseTemplate.find('img').attr('src', course.course_img_url);
+        $courseTemplate.find('a').attr('href', course.course_url);
+        $courseTemplate.find('.course-description a').append(course.title.slice(0, 36));
+        // $courseTemplate.find('.course-description p').append(school.slice(0, 39));
+        if (course.start_date === null) {
+          $courseTemplate.find('.course-description .date-display').text("Start Date: Self-Paced")
+        } else {
+          $courseTemplate.find('.course-description .date-display').text("Start Date: " + course.start_date)
+        }
+        // coursetemplate should now be appended into container
+        $('.courses-container').append($courseTemplate.attr('class', 'course'));
+      });
+      return $courseTemplate
+    },
+
+    submitQuery: function(searchTerms) {
       $.ajax({
-        url: "/courses/search/",
-        data: "ruby"
-      }).done(function() {
-        alert('sending search query successful');
-      }).fail(function() {
-        alert('sending search query failed');
+        url: "/courses/search/" + searchTerms
+      }).done(function(data) {
+        Search.emptyCourseContainer();
+        Search.appendResults(data);
+      }).fail(function(data) {
+        console.log('sending search query failed');
       });
     }
   }
